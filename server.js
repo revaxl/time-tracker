a374eafcb74636b973fb3f9c713687944d10df29
revaxl <robin208@hotmail.fr>
2018-06-01 14:46:27 +0100

    updated server to connect to mlab database

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const taskRoutes = require('./routes/task');
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/tracker', (err) => {
+mongoose.connect('mongodb://user:timetracker1@ds247077.mlab.com:47077/time-tracker', (err) => {
 	if (err) throw err;
 	console.log('connected to db');
 });
@@ -15,6 +16,6 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.use('/api', taskRoutes);
 
-app.listen(8000, () => {
-	console.log('working on port 3000');
+app.listen(PORT, () => {
+	console.log(`working on port ${PORT}`);
 });
